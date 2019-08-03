bd134be117b4a25fc26c071906c6245e9f3b7e95
Humberto Peluso <humbertopeluso@yahoo.com.br>
2019-08-03 13:18:32 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -56,6 +56,11 @@ public class CursomcApplication implements CommandLineRunner {
 
         Categoria cat1 = new Categoria( "Informática");
         Categoria cat2 = new Categoria("Escritório");
+        Categoria cat3 = new Categoria("Cama mesa e banho");
+        Categoria cat4 = new Categoria( "Eletrônicos");
+        Categoria cat5 = new Categoria( "Jardinagem");
+        Categoria cat6 = new Categoria("Decoração");
+        Categoria cat7 = new Categoria( "Perfumaria");
 
         Produto p1 = new Produto("Computador", 2000.00);
         Produto p2 = new Produto("Impressora 800", 800.00);
@@ -64,12 +69,14 @@ public class CursomcApplication implements CommandLineRunner {
         cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
         cat2.getProdutos().addAll(Arrays.asList(p2));
 
+
         p1.getCategorias().addAll(Arrays.asList(cat1));
         p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
         p3.getCategorias().addAll(Arrays.asList(cat1));
 
 
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
         produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
         Estado est1 = new Estado("Minas Gerais");
--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -6,7 +6,11 @@ import com.nelioalves.cursomc.services.exceptions.DataIntegrityException;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -45,12 +49,17 @@ public class CategoriaService {
             throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 
         }
-
-
     }
 
     public List<Categoria>findAll(){
         return categoriaRepository.findAll();
     }
 
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+
+        PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+
+        return categoriaRepository.findAll(pageRequest);
+
+    }
 }
