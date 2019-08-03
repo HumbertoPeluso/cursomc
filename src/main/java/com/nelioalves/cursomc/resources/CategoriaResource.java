15cd7596bd2ebf13b421d70ad2043900ea202ca8
Humberto Peluso <humbertopeluso@yahoo.com.br>
2019-08-03 12:43:41 -0300

    listando todas as categorias com dto

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.nelioalves.cursomc.resources;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.dto.CategoriaDTO;
 import com.nelioalves.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/categorias")
@@ -49,6 +52,17 @@ public class CategoriaResource {
         Categoria obj = categoriaService.find(id);
         categoriaService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaDTO>> findAll(){
+
+      List<Categoria> list = categoriaService.findAll();
+      List<CategoriaDTO>listDto = list.stream()
+              .map(obj -> new CategoriaDTO(obj))
+              .collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(listDto);
     }
 }
 
--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -46,6 +47,10 @@ public class CategoriaService {
         }
 
 
+    }
+
+    public List<Categoria>findAll(){
+        return categoriaRepository.findAll();
     }
 
 }
