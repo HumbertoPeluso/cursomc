050a8d52a3658a8d0627d3fb0a92765a94129539
Humberto Peluso <humbertopeluso@yahoo.com.br>
2019-08-14 15:47:19 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -22,15 +22,6 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
-    public Categoria insert(Categoria obj){
-        return categoriaRepository.save(obj);
-    }
-
-    public Categoria update(Categoria obj) {
-        find(obj.getId());
-        return categoriaRepository.save(obj);
-    }
-
     public Categoria find(int id){
 
        /* Optional<Categoria> obj = categoriaRepository.findById(id);
@@ -39,6 +30,15 @@ public class CategoriaService {
         Optional<Categoria> obj = categoriaRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(
                 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+    }
+
+    public Categoria insert(Categoria obj){
+        return categoriaRepository.save(obj);
+    }
+
+    public Categoria update(Categoria obj) {
+        find(obj.getId());
+        return categoriaRepository.save(obj);
     }
 
     public void delete(Integer id){
--- src/main/java/com/nelioalves/cursomc/services/ClienteService.java
+++ src/main/java/com/nelioalves/cursomc/services/ClienteService.java
@@ -1,13 +1,20 @@
 package com.nelioalves.cursomc.services;
 
-import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.domain.Cliente;
-import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.dto.ClienteDTO;
 import com.nelioalves.cursomc.repositories.ClienteRepository;
+import com.nelioalves.cursomc.repositories.ClienteRepository;
+import com.nelioalves.cursomc.services.exceptions.DataIntegrityException;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,15 +23,57 @@ public class ClienteService {
     @Autowired
     private ClienteRepository clienteRepository;
 
-    public Cliente find(int id){
+    public Cliente find(int id) {
 
-       /* Optional<Categoria> obj = categoriaRepository.findById(id);
+       /* Optional<Cliente> obj = clienteRepository.findById(id);
         return obj.orElse(null);*/
 
         Optional<Cliente> obj = clienteRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(
                 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 
+    }
+
+    public Cliente update(Cliente obj) {
+        Cliente newObj = find(obj.getId());
+
+        updateData(newObj, obj);
+
+        return clienteRepository.save(newObj);
+    }
+
+    private void updateData(Cliente newObj, Cliente obj) {
+
+        newObj.setNome(obj.getNome());
+        newObj.setEmail(obj.getEmail());
+
 
     }
+
+    public void delete(Integer id) {
+        find(id);
+        try {
+            clienteRepository.deleteById(id);
+
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+        }
+    }
+
+    public List<Cliente> findAll() {
+        return clienteRepository.findAll();
+    }
+
+    public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+
+        return clienteRepository.findAll(pageRequest);
+    }
+
+    public Cliente fromDTO(ClienteDTO objDto) {
+        return new Cliente(objDto.getNome(), objDto.getEmail(), null, null);
+    }
+
+
 }
