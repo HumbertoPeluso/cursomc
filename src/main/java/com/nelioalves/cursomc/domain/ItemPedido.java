329ef4a912c00303eb0b3b97b7bb98c6d99eb1b6
Humberto Peluso <humbertopeluso@yahoo.com.br>
2019-08-16 09:21:26 -0300

    total e subtotal do pedido

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -75,7 +75,9 @@ public class ItemPedido implements Serializable {
         return id.getProduto();
     }
 
-
+    public double getSubTotal() {
+        return (preco - desconto) * quantidade;
+    }
 
     @Override
     public boolean equals(Object o) {
