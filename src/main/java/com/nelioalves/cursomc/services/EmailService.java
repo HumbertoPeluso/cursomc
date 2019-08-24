34b5d985503b19de8c79a48303b61d09c7f9857f
Humberto Peluso <humbertopeluso@yahoo.com.br>
2019-08-24 14:38:44 -0300

    Esqueci a senha

--- src/main/java/com/nelioalves/cursomc/services/EmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/EmailService.java
@@ -1,5 +1,6 @@
 package com.nelioalves.cursomc.services;
 
+import com.nelioalves.cursomc.domain.Cliente;
 import com.nelioalves.cursomc.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
@@ -8,5 +9,7 @@ public interface EmailService {
     void sendOrderConfirmationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage msg);
+
+    void sendNewPasswordEmail(Cliente cliente, String newPass);
 
 }
