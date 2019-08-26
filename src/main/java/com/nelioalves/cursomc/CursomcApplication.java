7bc1408251cdde273bda1d9bafe0d7d236a1b39a
Humberto Peluso <humbertopeluso@yahoo.com.br>
2019-08-26 20:20:45 -0300

    Enviando imagem via endpoint

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,25 +1,12 @@
 package com.nelioalves.cursomc;
 
-import com.nelioalves.cursomc.domain.*;
-import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
-import com.nelioalves.cursomc.domain.enums.TipoCliente;
-import com.nelioalves.cursomc.repositories.*;
-import com.nelioalves.cursomc.services.S3Service;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.amqp.AbstractRabbitListenerContainerFactoryConfigurer;
-
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-    @Autowired
-    private S3Service s3Service;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
@@ -28,6 +15,6 @@ public class CursomcApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        s3Service.uploadFile("/home/humberto/Pictures/locacao-de-computadores-para-escritorios-01.jpg");
+
     }
 }
